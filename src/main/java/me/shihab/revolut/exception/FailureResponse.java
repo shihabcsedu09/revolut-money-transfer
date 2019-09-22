ad98d306b2234849e84445fbda137d668af1d36c
Shihab Rahman <shihab.csedu09@gmail.com>
2019-09-22 20:57:06 +0600

    Add setters for failure message

--- src/main/java/me/shihab/revolut/exception/FailureResponse.java
+++ src/main/java/me/shihab/revolut/exception/FailureResponse.java
@@ -13,17 +13,8 @@ public class FailureResponse {
         return statusCode;
     }
 
-    public void setStatusCode(int statusCode) {
-        this.statusCode = statusCode;
-    }
-
     public String getMessage() {
         return message;
     }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
 
 }
