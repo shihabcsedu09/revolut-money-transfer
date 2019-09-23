e351e7d16d7a4b88ada2aebfc62db1df2384a58d
Shihab Rahman <shihab.rahman@infolytx.com>
2019-09-23 21:14:50 +0600

    Add validation in get account by id resource method

--- src/main/java/me/shihab/revolut/resources/AccountResource.java
+++ src/main/java/me/shihab/revolut/resources/AccountResource.java
@@ -1,13 +1,17 @@
 package me.shihab.revolut.resources;
 
 import io.dropwizard.hibernate.UnitOfWork;
+import io.dropwizard.jersey.params.LongParam;
 import me.shihab.revolut.api.AccountDTO;
 import me.shihab.revolut.exception.RuntimeException;
 import me.shihab.revolut.service.AccountService;
+import org.hibernate.validator.valuehandling.UnwrapValidatedValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
@@ -24,9 +28,9 @@ public class AccountResource {
     @Path("/{accountId}")
     @GET
     @UnitOfWork
-    public AccountDTO getAccount(@PathParam("accountId") long accountId) throws RuntimeException {
+    public AccountDTO getAccount(@UnwrapValidatedValue @NotNull @Min(0) @PathParam("accountId") LongParam accountId) throws RuntimeException {
         LOGGER.info("Received account fetch request for account id: {}", accountId);
-        return accountService.get(accountId);
+        return accountService.get(accountId.get());
     }
 
     @POST
