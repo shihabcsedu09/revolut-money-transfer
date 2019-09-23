de882a04f81a809094062f82810ba1e6d868e3f1
Shihab Rahman <shihab.rahman@infolytx.com>
2019-09-23 21:06:53 +0600

    Add account already updated failure message

--- src/main/java/me/shihab/revolut/exception/FailureMessage.java
+++ src/main/java/me/shihab/revolut/exception/FailureMessage.java
@@ -4,7 +4,8 @@ public enum FailureMessage {
     ACCOUNT_NOT_FOUND("No account found for this id"),
     SOURCE_ACCOUNT_NOT_FOUND("Source account not found for this id"),
     DESTINATION_ACCOUNT_NOT_FOUND("Destination account not found for this id"),
-    NOT_ENOUGH_BALANCE("Not enough balance for this account");
+    NOT_ENOUGH_BALANCE("Not enough balance for this account"),
+    ACCOUNT_ALREADY_UPDATED("Your account was updated or deleted by another transaction");
 
     private String message;
 
--- src/main/java/me/shihab/revolut/exception/FailureStatusCode.java
+++ src/main/java/me/shihab/revolut/exception/FailureStatusCode.java
@@ -5,7 +5,9 @@ public enum FailureStatusCode {
     ACCOUNT_NOT_FOUND(600),
     SOURCE_ACCOUNT_NOT_FOUND(601),
     DESTINATION_ACCOUNT_NOT_FOUND(602),
-    NOT_ENOUGH_BALANCE(603);
+    NOT_ENOUGH_BALANCE(603),
+    ACCOUNT_ALREADY_UPDATED(604);
+
 
     private int statusCode;
 
